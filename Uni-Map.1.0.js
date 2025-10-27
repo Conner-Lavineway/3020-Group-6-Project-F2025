2283ecba62c8a24ef1e17f26644dd7a68c336f0e
Conner Lavineway <63662176+Conner-Lavineway@users.noreply.github.com>
2025-10-27 09:26:41 -0500

    Maptiler Functionality
    Text me if you want the api key, this is a test for maptiler functionality

--- Uni-Map.1.0.js
+++ Uni-Map.1.0.js
@@ -1,7 +1,11 @@
 
-const key = 'Redacted';
+const tile = '019a1752-72aa-797d-a3c7-b3c094cf97e3'
 const map = L.map('map').setView([49.808773, -97.13208],17);
 
+    const mtLayer = L.maptiler.maptilerLayer({
+    apiKey: key,
+    style: L.maptiler.$tile, //optional
+    }).addTo(map);
 
 map.setMaxBounds([
     [49.80725, -97.15431],
@@ -13,13 +17,13 @@ map.setMaxBounds([
 
 map.setMinZoom(15);
 map.setMaxZoom(19);
-
+/**
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
+*/
 
 function onMapClick(e) {
     alert("You clicked the map at " + e.latlng);
