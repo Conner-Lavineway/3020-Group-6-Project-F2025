222c98cb217acf04febb367bb7f46b3665fbf3a6
Conner Lavineway <63662176+Conner-Lavineway@users.noreply.github.com>
2025-11-03 21:05:16 -0600

    Added dropdowns
    Sorting and event dropdowns

--- dropdown.js
+++ dropdown.js
@@ -1,17 +1,80 @@
 function eventButton()
 {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) 
+    {
+        var openDropdown = dropdowns[i];
+        if(openDropdown.classList.contains('show')) 
+        {
+            openDropdown.classList.remove('show');
+        }
+        else
+        {
+            openDropdown.classList.remove('showSort');
+        }
+    }  
+
     document.getElementById("events").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbutton')) {
+function sortButton()
+{
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    for (i = 0; i < dropdowns.length; i++) 
+    {
+        var openDropdown = dropdowns[i];
+        if(openDropdown.classList.contains('show')) 
+        {
+            openDropdown.classList.remove('show');
+        }
+        else
+        {
+            openDropdown.classList.remove('showSort');
+        }
+    }  
+
+    document.getElementById("sorting").classList.toggle("showSort");
+}
+
+function filterButton()
+{
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) 
+    {
+        var openDropdown = dropdowns[i];
+        if(openDropdown.classList.contains('show')) 
+        {
+            openDropdown.classList.remove('show');
+        }
+        else
+        {
+            openDropdown.classList.remove('showSort');
+        }
+    }  
+    
+    document.getElementById("filters").classList.toggle("show")
+}
+
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbutton')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) 
+        {
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')) 
+            {
+                openDropdown.classList.remove('show');
+            }        
+            else
+            {
+                openDropdown.classList.remove('showSort');
+            }
+        }
     }
-  }
 }
