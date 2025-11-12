ffab4500a35d697346a367a3f1bd063bac749a33
Conner Lavineway <63662176+Conner-Lavineway@users.noreply.github.com>
2025-11-11 19:40:52 -0600

    Coomments

--- Assets/JS/populate.js
+++ Assets/JS/populate.js
@@ -1,22 +1,27 @@
-var filterDivs = document.getElementsByClassName('filter dropdown-content');
+var filterDivs = document.getElementsByClassName('filter dropdown-content'); //grab holder div by its classes
 
+//create the filter title div
 var titleDiv = document.createElement('div');
 titleDiv.className = 'filter-header dropdown-content';
 titleDiv.innerHTML = filterNames[0];
+//this can be done in a for loop for more div creations
 
+//create the detail holder div
 var detailDiv = document.createElement('div');
 detailDiv.className = 'filter-details dropdown-content'
 
+//append both divs to the holder div
 filterDivs[0].appendChild(titleDiv);
 filterDivs[0].appendChild(detailDiv);
 
+//create the buttons based on the array
 var i;
 for(i = 0; i < filters.amenities.length; i++)
 {
-    var button = document.createElement('button');
-    button.setAttribute("onclick", "filterButton()");
-    button.className = 'filterButton dropdown-content';
-    button.id = filters.amenities[i];
-    button.innerHTML = filters.amenities[i];
-    detailDiv.appendChild(button);
+    var button = document.createElement('button'); //create button
+    button.setAttribute("onclick", "filterButton()"); //make sure it is clickable by adding the onclick function
+    button.className = 'filterButton dropdown-content'; //give it the right classes
+    button.id = filters.amenities[i]; //give it an id
+    button.innerHTML = filters.amenities[i]; //display its name
+    detailDiv.appendChild(button); //add to details
 }
