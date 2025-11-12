4a0941c05a45ee8f6bc6620b75837d161a775e68
Conner Lavineway <63662176+Conner-Lavineway@users.noreply.github.com>
2025-11-11 23:29:50 -0600

    Most filters are now done with JS

--- Assets/JS/populate.js
+++ Assets/JS/populate.js
@@ -1,27 +1,52 @@
 var filterDivs = document.getElementsByClassName('filter dropdown-content'); //grab holder div by its classes
 
-//create the filter title div
-var titleDiv = document.createElement('div');
-titleDiv.className = 'filter-header dropdown-content';
-titleDiv.innerHTML = filterNames[0];
-//this can be done in a for loop for more div creations
 
-//create the detail holder div
-var detailDiv = document.createElement('div');
-detailDiv.className = 'filter-details dropdown-content'
+var i, x;
+var skip = 1;
 
-//append both divs to the holder div
-filterDivs[0].appendChild(titleDiv);
-filterDivs[0].appendChild(detailDiv);
+for(x = 0; x < filterDivs.length - skip; x++)
+{     
+    titleDiv = document.createElement('div');
+    titleDiv.className = 'filter-header dropdown-content';
+    titleDiv.innerHTML = filterNames[x];
 
-//create the buttons based on the array
-var i;
-for(i = 0; i < filters.amenities.length; i++)
-{
-    var button = document.createElement('button'); //create button
-    button.setAttribute("onclick", "filterButton()"); //make sure it is clickable by adding the onclick function
-    button.className = 'filterButton dropdown-content'; //give it the right classes
-    button.id = filters.amenities[i]; //give it an id
-    button.innerHTML = filters.amenities[i]; //display its name
-    detailDiv.appendChild(button); //add to details
+    //create the detail holder div
+    detailDiv = document.createElement('div');
+    detailDiv.className = 'filter-details dropdown-content'
+    
+    //append both divs to the holder div
+    filterDivs[x].appendChild(titleDiv);
+    filterDivs[x].appendChild(detailDiv);
+    
+
+    switch(x)
+    {
+        case 0:
+            //create the buttons based on the array
+            for(i = 0; i < filters.amenities.length; i++)
+            {
+                button = document.createElement('button'); //create button
+                button.setAttribute("onclick", "filterButton()"); //make sure it is clickable by adding the onclick function
+                button.className = 'filterButton dropdown-content'; //give it the right classes
+                button.id = filters.amenities[i]; //give it an id
+                button.innerHTML = filters.amenities[i]; //display its name
+                detailDiv.appendChild(button); //add to details
+            }
+        break;
+        case 1:
+            //create a select element
+            select = document.createElement('select');
+            select.className = 'building dropdown-content'
+            detailDiv.appendChild(select);
+
+            for(i = 0; i < filters.building.length; i++)
+            {
+                option = document.createElement('option'); //create a select option
+                option.className = 'dropdown-content'; //give it a class
+                option.value = i + 1; //set its value
+                option.innerHTML = filters.building[i]; //set its text
+                select.appendChild(option) //add it to the list
+            }
+        break;
+    }
 }
