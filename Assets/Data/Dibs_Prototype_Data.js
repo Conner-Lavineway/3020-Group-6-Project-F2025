89c2097b1364107a0907bdf2d2c9ac0290f64deb
Conner Lavineway <63662176+Conner-Lavineway@users.noreply.github.com>
2025-11-19 12:32:51 -0600

    Update Dibs_Prototype_Data.js

--- Assets/Data/Dibs_Prototype_Data.js
+++ Assets/Data/Dibs_Prototype_Data.js
@@ -635,6 +635,7 @@ function extractEvents(date = new Date(), rooms = ROOMS) {
     for (const room of rooms) {
         const eventList = room.events;
         for (const event of eventList) {
+            event.title += ": " + room.buildingName + " " + room.roomNumber;
             const startTime = new Date(event.startTime);
             // check year, month, day
             const sameDay =
